686304c7dafe088300f8523eab4ff31d20c1a650
Brendan McMahon <brendan6z@gmail.com>
2021-10-04 10:10:50 -0700

    api calls

--- js/weather.js
+++ js/weather.js
@@ -12,4 +12,29 @@ window.onload = function () {
     if (18 < hours && hours < 24) {
         console.log('Good Evening')
     }
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=San Diego,US&appid=4b82b3b40e9f7a693087eab5ee7b5044&units=imperial')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            // Set location
+            let locationInnerHTML = `<strong id="location">${data.name}</strong>, ${data.sys.country}`
+            document.querySelector('#location').innerHTML = locationInnerHTML
+
+            // Set weather condition
+            document.querySelector('#condition-text').textContent = data.weather[0].description
+
+            // Set Current tempurature
+            document.querySelector('#current-temp').textContent = Math.floor(data.main.temp) + '°'
+
+            // Wind Speed
+            document.querySelector('#wind').textContent = data.wind.speed + ' mph'
+
+            // Real Feel
+            document.querySelector('#feel').textContent = data.main.feels_like + '°'
+
+            // Pressure
+            document.querySelector('#pressure').textContent = data.main.pressure + '°'
+        })
+
 }
