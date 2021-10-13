230c95b6d3fd2b5c7cf8b06da3767abe77b6d3f6
yaffa7 <bmcmahon2@mail.csuchico.edu>
2021-10-13 12:02:28 -0700

    mobile fixes

--- js/weather.js
+++ js/weather.js
@@ -93,6 +93,8 @@ function AddSearchListener() {
 
             // Move Search
             document.querySelector('.search-container').classList.add('searched')
+
+            SetBackground()
         })
     })
 }
