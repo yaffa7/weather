316d0c3bcac22c3d3079d14fe23bb3926d431630
yaffa7 <bmcmahon2@mail.csuchico.edu>
2021-10-23 10:51:57 -0700

    added hourly forecast

--- js/weather.js
+++ js/weather.js
@@ -26,11 +26,14 @@ window.onload = function () {
 
 function WeatherSearch(lat, lon) {
     // start spinner
-        // Call open weather with lat/long
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APPID}&units=imperial`)
-            .then(response => response.json())
-            .then(data => { SetUIWithData(data.current)})
-            // stop spinner
+    // Call open weather with lat/long
+    LocationFromCoords(lat, lon)
+        .then(location => {
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APPID}&units=imperial`)
+                .then(response => response.json())
+                .then(data => { SetUIWithData(data, location) })
+        })
+    // stop spinner
 }
 
 function SetBackground() {
@@ -60,46 +63,46 @@ function SetBackground() {
     document.querySelector('#current-date').textContent = dateString
 }
 
-function SetUIWithData(data) {
-    console.log(data)
-    if (data.cod == 404) {
+function SetUIWithData(data, location) {
+    console.log(data, data.current)
+    if (data.current.cod == 404) {
         console.log('Error detected')
         document.querySelector('#error').classList.remove('hidden')
-        document.querySelector('#error').textContent = data.message
+        document.querySelector('#error').textContent = data.current.message
     }
     // Set Image
-    let imageHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="" width="100px" height="100px">`
+    let imageHTML = `<img src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt="weather icon">`
     document.querySelector('#image-container').innerHTML = imageHTML
 
     // Set location
-    let locationInnerHTML = `<strong id="location">${data.name}</strong>, US`
+    let locationInnerHTML = `<strong id="location">${location}</strong>, US`
     document.querySelector('#location').innerHTML = locationInnerHTML
 
     // Set weather condition
-    document.querySelector('#condition-text').textContent = data.weather[0].description
+    document.querySelector('#condition-text').textContent = data.current.weather[0].description
 
     // Set Current tempurature
-    document.querySelector('#current-temp').textContent = Math.floor(data.temp) + '°'
+    document.querySelector('#current-temp').textContent = Math.floor(data.current.temp) + '°'
 
     // Wind Speed
-    document.querySelector('#wind').textContent = data.wind_speed + ' mph'
+    document.querySelector('#wind').textContent = data.current.wind_speed + ' mph'
 
     // Real Feel
-    document.querySelector('#feel').textContent = data.feels_like + '°'
+    document.querySelector('#feel').textContent = data.current.feels_like + '°'
 
     // Pressure
-    document.querySelector('#pressure').textContent = data.pressure + '°'
+    document.querySelector('#pressure').textContent = data.current.pressure + '°'
 
     // Sunrise
-    let sunrise = new Date(data.sunrise * 1000)
+    let sunrise = new Date(data.current.sunrise * 1000)
     document.querySelector('#sunrise').textContent = to12Hour(sunrise)
 
     // Sunset
-    let sunset = new Date(data.sunset * 1000)
+    let sunset = new Date(data.current.sunset * 1000)
     document.querySelector('#sunset').textContent = to12Hour(sunset)
 
     // Humidity
-    document.querySelector('#humidity').textContent = data.humidity + '%'
+    document.querySelector('#humidity').textContent = data.current.humidity + '%'
 
     // Show Panel
     document.querySelector('.main-panel').classList.remove('hidden')
@@ -108,7 +111,31 @@ function SetUIWithData(data) {
     document.querySelector('.search-container').classList.add('searched')
 
     // Show Search Again button
-    searchAgain.classList.remove('hidden')
+    document.querySelector('#search-again').classList.remove('hidden')
+
+
+    // Create hourly weather
+    const hourComponent = (data) => {
+        let date = new Date(data.dt * 1000)
+        let parsedDate = to12Hour(date, true)
+        return `
+                <div class="high">${Math.floor(data.temp)}°</div>
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="small weather icon" width="40px">
+                <div class="low">${parsedDate}</div>
+        `
+    }
+
+    // Remove previous elements
+    document.querySelector('#hourly').innerHTML = ''
+    
+    for (let i = 0; i < 5; i++) {
+        let elem = document.createElement('div')
+        elem.id = 'hour'
+        elem.classList.add('hour')
+        elem.innerHTML = hourComponent(data.hourly[i])
+        if (i == 0) { elem.classList.add('current') }
+        document.querySelector('#hourly').appendChild(elem)
+    }
 }
 
 function AddSearchListener() {
@@ -131,27 +158,37 @@ function AddSearchListener() {
 
         fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=1&appid=${APPID}&units=imperial`)
             .then(response => response.json())
-            .then(data => { WeatherSearch(data[0].lat, data[0].lon)})
+            .then(data => WeatherSearch(data[0].lat, data[0].lon, input.value))
     })
 }
 
 
-function to12Hour(date) {
+function to12Hour(date, onlyHour = false) {
     let hours = ''
     let affix = ''
     let minutes = date.getMinutes()
-    if(date.getHours() <= 12) {
+    if (date.getHours() <= 12) {
         hours = date.getHours()
         affix = 'AM'
     }
-    if(date.getHours() > 12) {
+    if (date.getHours() > 12) {
         hours = date.getHours() % 12
         affix = 'PM'
     }
 
-    if(date.getMinutes() < 10) {
+    if (date.getMinutes() < 10) {
         minutes = `0${date.getMinutes()}`
-    } 
+    }
 
-    return `${hours}:${minutes} ${affix}`
+    if (onlyHour) {
+        return `${hours} ${affix}`
+    } else {
+        return `${hours}:${minutes} ${affix}`
+    }
+}
+
+function LocationFromCoords(lat, lon) {
+    return fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${APPID}&units=imperial`)
+        .then(response => response.json())
+        .then(data => data[0].name)
 }
